37d5511706c57ec46a9f2db2d69004794ef2e475
JoeyTribbni <87077732+JoeyTribbni@users.noreply.github.com>
2021-07-13 11:25:47 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,66 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
     let targetImage = document.querySelector("#smart-image");
-    
-    targetImage.addEventListener("click", function() {  
+
+    targetImage.addEventListener("click", function() {
         if (targetImage.classList.contains("small")) {
             targetImage.classList.remove("small");
         } else {
             targetImage.classList.add("small");
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+let targetImage1= document.querySelector("#smart-image1");
+
+targetImage1.addEventListener("click", function() {
+    if (targetImage1.classList.contains("small")) {
+        targetImage1.classList.remove("small");
+    } else {
+        targetImage1.classList.add("small");
+    }
+});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage2= document.querySelector("#smart-image2");
+
+    targetImage2.addEventListener("click", function() {
+        if (targetImage2.classList.contains("small")) {
+            targetImage2.classList.remove("small");
+        } else {
+            targetImage2.classList.add("small");
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage3= document.querySelector("#smart-image3");
+
+    targetImage3.addEventListener("click", function() {
+        if (targetImage3.classList.contains("small")) {
+            targetImage3.classList.remove("small");
+        } else {
+            targetImage3.classList.add("small");
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage4= document.querySelector("#smart-image4");
+
+    targetImage4.addEventListener("click", function() {
+        if (targetImage4.classList.contains("small")) {
+            targetImage4.classList.remove("small");
+        } else {
+            targetImage4.classList.add("small");
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage5= document.querySelector("#smart-image5");
+
+    targetImage5.addEventListener("click", function() {
+        if (targetImage5.classList.contains("small")) {
+            targetImage5.classList.remove("small");
+        } else {
+            targetImage5.classList.add("small");
         }
     });
 });
